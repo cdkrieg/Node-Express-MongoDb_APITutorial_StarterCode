156c95d7daa9fb1e77b8c121c6c2fd2c0f9e6103
Christopher Krieg <christopherkrieg@ChristohersiMac.attlocal.net>
2022-04-11 20:46:46 -0500

    added PUT and DELETE methods to routes

--- routes/products.js
+++ routes/products.js
@@ -31,6 +31,42 @@ router.get("/:productId", async (req, res) => {
   }
 });
 
+// http://localhost:3007/api/products/:productId
+router.put("/:productId", async (req, res) => {
+  try {
+    const { error } = validateProduct(req.body);
+    if (error) return res.status(400).send(error);
+
+    let product = await Product.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      { new: true }
+    );
+    if (!product)
+      return res
+        .status(400)
+        .send(`Product with Id of ${req.params.productId} does not exist`);
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+// http://localhost:3007/api/products/:productId
+router.delete("/:productId", async (req, res) => {
+    try {
+      let product = await Product.findByIdAndDelete(req.params.productId);
+      if (!product) {
+        return res
+          .status(400)
+          .send(`Product with Id of ${req.params.productId} does not exist`);
+      }
+      return res.status(200).send(product);
+    } catch (error) {
+      return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+  });
+
 // http://localhost:3007/api/products
 router.post("/", async (req, res) => {
   try {
