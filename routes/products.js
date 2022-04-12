519735b6e89de0322f0370eb143975b650203324
Christopher Krieg <christopherkrieg@ChristohersiMac.attlocal.net>
2022-04-11 20:04:33 -0500

    added POST method with validation, added JOI node for validation

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,18 @@
-const product = require("../models/product");
+const products = require("../models/product");
 const express = require("express");
+const {Product, validateProduct} = require("../models/product");
 const router = express.Router();
 
-
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateProduct(req.body);
+    if (error) return res.status(400).send(error);
+    let newProduct = await new Product(req.body);
+    await newProduct.save();
+    return res.status(201).send(newProduct);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
 
 module.exports = router;
